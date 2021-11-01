0ce1c92e80e55c8d29f21b30a88153fafd3bc8c7
Zhang-Haoran <haoranzhang001@gmail.com>
2021-11-01 14:43:16 +1100

    rename

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../../services/hero/hero';
-import {HEROES} from '../../mock-heroes';
+import {HEROES} from '../../mock-heroes.data';
 import { HeroService } from '../../services/hero/hero.service';
 import { MessageService } from '../../services/message/message.service';
 
